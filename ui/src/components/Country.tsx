2d307b24c8b9805ff9a2bdfcd0972a950f8facea
dakshithal <dakshithalenagala@gmail.com>
2022-10-08 21:20:39 +0300

    Add basic ui structure

---
+++ ui/src/components/Country.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material"
+import { Container } from "@mui/system"
+
+export const Country = () => {
+  return (
+    <Container id="country-details">
+      <Typography variant="h2">
+        Country Details
+      </Typography>
+    </Container>
+  )
+}
---
+++ ui/src/components/CountryList.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material"
+import { Container } from "@mui/system"
+
+export const CountryList = () => {
+  return (
+    <Container>
+      <Typography variant="h2">Countries</Typography> 
+    </Container>
+  )
+}
