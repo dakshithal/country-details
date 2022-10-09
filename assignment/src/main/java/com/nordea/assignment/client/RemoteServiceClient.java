73e49c803180f798883a112c1a742da0a12ceb77
dakshithal <dakshithalenagala@gmail.com>
2022-10-09 03:13:02 +0300

    Add docker file, resolve connection issue on docker

--- assignment/src/main/java/com/nordea/assignment/client/RemoteServiceClient.java
+++ assignment/src/main/java/com/nordea/assignment/client/RemoteServiceClient.java
@@ -4,6 +4,10 @@ import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,6 +21,10 @@ public class RemoteServiceClient {
     @Retry(name = clientIdentifier)
     @Bulkhead(name = clientIdentifier)
     public <T> T getForObject(String url, Class<T> responseType) {
-        return restTemplate.getForObject(url, responseType);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.USER_AGENT, clientIdentifier);
+        HttpEntity<Void> request = new HttpEntity<>(headers);
+        ResponseEntity<T> responseEntity = restTemplate.exchange(url, HttpMethod.GET, request, responseType);
+        return responseEntity.getBody();
     }
 }
--- assignment/src/test/java/com/nordea/assignment/service/ResilienceTest.java
+++ assignment/src/test/java/com/nordea/assignment/service/ResilienceTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -40,22 +41,36 @@ public class ResilienceTest {
     @Test
     public void doesNotCallRemoteServiceWhenCircuitBreakerIsOpen() {
         CircuitBreaker circuitBreaker = circuitBreakerRegistry.circuitBreaker(remoteClient.clientIdentifier);
+        // Open circuit breaker
         circuitBreaker.transitionToOpenState();
+        // Verify that an exception is thrown when calling the external service
         assertThrows(CallNotPermittedException.class, () -> countryService.retrieveCountry(COUNTRY_1_NAME));
+        // Verify that the remote service is not called
         verify(restTemplate, times(0)).getForObject(anyString(), any());
     }
 
     @Test
     public void retryWhenThereIsAnUnexpectedException() {
         CircuitBreaker circuitBreaker = circuitBreakerRegistry.circuitBreaker(remoteClient.clientIdentifier);
+        // Close circuit breaker
         circuitBreaker.transitionToClosedState();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.USER_AGENT, remoteClient.clientIdentifier);
+        HttpEntity<Void> request = new HttpEntity<>(headers);
         CountryListResponse retryResponse = new CountryListResponse();
+        ResponseEntity<CountryListResponse> responseEntity = ResponseEntity.ok().body(retryResponse);
         retryResponse.setMsg(RETRY_RESPONSE_MSG);
-        Mockito.when(restTemplate.getForObject(TEST_URL, CountryListResponse.class))
+
+        // Mock object throws exception first, then returns a successful response
+        Mockito.when(restTemplate.exchange(TEST_URL, HttpMethod.GET, request, CountryListResponse.class))
                 .thenThrow(new RuntimeException("Test Retry"))
-                .thenReturn(retryResponse);
+                .thenReturn(responseEntity);
+        // Call remote service only once
         CountryListResponse response = remoteClient.getForObject(TEST_URL, CountryListResponse.class);
+        // Verify that the service is called twice (retried after exception)
+        verify(restTemplate, times(2)).
+                exchange(TEST_URL, HttpMethod.GET, request, CountryListResponse.class);
         assertEquals(RETRY_RESPONSE_MSG, response.getMsg());
-        verify(restTemplate, times(2)).getForObject(anyString(), any());
     }
 }
